57036f1d5fc95715041bef860ef6d0ffdc87ce71
Tareq Annan <dmtareq45@gmail.com>
2023-06-13 18:45:15 +0600

    Using Class methods this code has implemented.

--- clear_the_clutter/file_operation_2.py
+++ clear_the_clutter/file_operation_2.py
@@ -53,6 +53,8 @@ class file_operation:
                     print('File creation unsuccessful because your desired file creating size is equal or less than existed file')
 
 
+
+
 class file_operation_delete(file_operation):
     def __init__(self, name, location, sub_location, types, size):
         super().__init__(name, location, sub_location, types, size)
@@ -80,9 +82,24 @@ class file_operation_delete(file_operation):
                 os.rmdir(Path)  # No need to handle error here, because while this block is executing, that time this should be empty
                 # shutil.rmtree(Path, ignore_errors=True)
 
+    def rename_files(self, new_name, old_file_type):
+        Path = os.path.join(self.location, self.sub_location)
+        files_lst = os.listdir(Path)
+        # file_type = input('What type of files do you want to make order? ')
+        # desired_file_lst = list()
+        # self.types = file_type
+
+        i = 0
+        for item in files_lst:
+            if old_file_type in item:
+                # desired_file_lst.append(item)
+                os.rename(f'{Path}/{item}', f'{Path}/{new_name}{i}.{old_file_type}')
+                i += 1
+        print('Done!!!')
+
 
 while True:
-    question = input('What you want to do?\n 1. add \n 2. delete \n 3. exit \n')
+    question = input('What you want to do?\n 1. add \n 2. delete \n 3. Rename \n 4. exit \n')
     if question == '1' or question.lower() == 'add':
         name = input('Please enter FILES name: ')
         folder_name = input('Please enter FOLDER name : ')
@@ -102,9 +119,20 @@ while True:
         if ques.lower == 'yes':
             del_files = file_operation_delete(name, folder_name, sub_folder_name, extension_name, size = 0)
             del_files.delete_all_files()
+
         else:
             seq = input('Enter files sequence number : ')
             del_file = file_operation_delete(name, folder_name, sub_folder_name, extension_name, size=0)
             del_file.delete_file(seq)
-    elif question == '3' or question == 'exit':
+
+    elif question == '3' or question == 'rename':
+        new_file_name = input('Enter new file name : ')
+        type_of_file = input('Enter what types of file you want to edit EXAMPLE: .txt : ')
+        loc = input('Enter location name : ')
+        sub_loc = input('Enter sub location name : ')
+
+        d = file_operation_delete(name='', location=loc, sub_location=sub_loc, types='', size=0)
+        d.rename_files(new_file_name, type_of_file)
+
+    elif question == '4' or question == 'exit':
         break
